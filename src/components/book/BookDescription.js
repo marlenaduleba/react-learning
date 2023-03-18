5f3584fc5e461b27e5aedad0dd0cc943fb326fdb
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-18 14:25:39 +0100

    working with state

--- src/components/book/BookDescription.js
+++ src/components/book/BookDescription.js
@@ -1,10 +1,9 @@
 const BookDescription = (props) => {
   return (
     <div>
-    <h1>{props.title}</h1>
-    <h2>{props.author}</h2>
-    <h4>Pages: {props.pages}</h4>
-    <button onClick={() => props.deleteBook(props.id)}></button>
+    <h1>{props.book.title}</h1>
+    <h2>{props.book.author}</h2>
+    <h4>Pages: {props.book.pages}</h4>
     </div>
   );
         
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Counter from "./components/counter/Counter";
+import Book from "./pages/book/Book";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <Book />
   </React.StrictMode>
 );
 
