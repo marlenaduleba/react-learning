a5c067f13963e4604a35734bcfd4ca1dc4ed3ec4
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-18 13:58:54 +0100

    Counter component created

---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,38 @@
+import React, {Component} from "react";
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        };
+    }
+
+    increase = () => {
+        this.setState((prevState) => ({counter: prevState.counter + 1}));
+    }
+
+    decrease = () => {
+        this.setState((prevState) => ({counter: prevState.counter - 1}));
+    }
+
+    clear = () => {
+        this.setState({counter: 0});
+    }
+
+    render() {
+        return (
+            <div>
+                Stan licznika: {this.state.counter}
+                <br />
+                <button onClick={this.increase}>Zwiększ o jeden</button>
+                <br />
+                <button onClick={this.decrease}>Zmniejsz o jeden</button>
+                <br />
+                <button onClick={this.clear}>Zeruj</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Book from "./pages/book/Book";
+import Counter from "./components/counter/Counter";
 
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Book />
+    <Counter />
   </React.StrictMode>
 );
-
 
 
 // If you want to start measuring performance in your app, pass a function
