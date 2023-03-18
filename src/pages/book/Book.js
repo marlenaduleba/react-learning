b48959cca580c77e88708442baa703a88ce407c3
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-18 10:22:44 +0100

    book list rendered

--- src/pages/book/Book.js
+++ src/pages/book/Book.js
@@ -4,11 +4,47 @@ import Footer from "../../components/partial/Footer";
 import BookDescription from "../../components/book/BookDescription";
 
 class Book extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [
+        {
+          title: "Hobbit",
+          author: "J. R. Tolkien",
+          pages: "455",
+        },
+        {
+          title: "Rok 1984",
+          author: "G. Orwell",
+          pages: "455",
+        },
+        {
+          title: "Władca Pierścieni",
+          author: "J. R. Tolkien",
+          pages: "669",
+        },
+        {
+          title: "Stary człowiek i morze",
+          author: "E. Hemingway",
+          pages: "226",
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div>
         <Header />
-        <BookDescription />
+
+        {this.state.books.map((book, i) => (
+          <BookDescription
+            key={i}
+            title={book.title}
+            author={book.author}
+            pages={book.pages}
+          />
+        ))}
+
         <Footer />
       </div>
     );
