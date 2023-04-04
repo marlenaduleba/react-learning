6f3b3dcefb1a12f14b2bad146703b76038120cdb
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-04 13:39:00 +0200

    StrictMode added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 import { App } from "./App";
 
 import reportWebVitals from "./reportWebVitals";
-import { UserProvider } from "./userContext";
+//import { UserProvider } from "./userContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-    <UserProvider>
-      <App />
-    </UserProvider>
-  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
