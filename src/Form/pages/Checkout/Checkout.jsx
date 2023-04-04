deefe94c962d67ce29c1c6288a2b7dcf709e87e7
Marlena Dulęba <marlena_duleba@o2.pl>
2023-04-04 07:46:41 +0200

    Checkout finished

--- src/Form/pages/Checkout/Checkout.jsx
+++ src/Form/pages/Checkout/Checkout.jsx
@@ -6,18 +6,21 @@ class Checkout extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        firstname: "",
-        lastname: ""
-    }
+      firstname: "",
+      lastname: "",
+      street: "",
+      zip: "", 
+      city: ""
+    };
   }
 
-  changeHandler = event => {
+  changeHandler = (event) => {
     let inputName = event.target.name;
     let inputValue = event.target.value;
     this.setState({
-        [inputName]: inputValue
-    })
-  }
+      [inputName]: inputValue,
+    });
+  };
 
   render() {
     return (
@@ -32,20 +35,84 @@ class Checkout extends Component {
             </Row>
             <Row>
               <Col xs={12} md={4}>
-                <MyInput type="text" name="firstname" label="Imię" className="form-control" value={this.state.firstname} onChange={this.changeHandler} />
+                <MyInput
+                  type="text"
+                  name="firstname"
+                  label="Imię"
+                  className="form-control"
+                  value={this.state.firstname}
+                  onChange={this.changeHandler}
+                />
               </Col>
               <Col xs={12} md={4}>
-                <MyInput type="text" name="lastname" label="Nazwisko" className="form-control" value={this.state.lastname} onChange={this.changeHandler} />
+                <MyInput
+                  type="text"
+                  name="lastname"
+                  label="Nazwisko"
+                  className="form-control"
+                  value={this.state.lastname}
+                  onChange={this.changeHandler}
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <MyInput
+                  type="text"
+                  name="street"
+                  label="Ulica i nr domu"
+                  className="form-control"
+                  value={this.state.street}
+                  onChange={this.changeHandler}
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <MyInput
+                  type="text"
+                  name="zip"
+                  label="Kod pocztowy"
+                  className="form-control"
+                  value={this.state.zip}
+                  onChange={this.changeHandler}
+                />
+              </Col>
+              <Col xs={12} md={4}>
+                <MyInput
+                  type="text"
+                  name="city"
+                  label="Miasto"
+                  className="form-control"
+                  value={this.state.city}
+                  onChange={this.changeHandler}
+                />
               </Col>
             </Row>
           </div>
         </form>
         <Row>
-            <Col>
-                <h2>Podane dane:</h2>
+          <Col>
+            <h2>Podane dane:</h2>
+            <ul className="list-group">
+              <li className="list-group-item">
                 Imię:{" "}
                 {this.state.firstname == "" ? "N/A" : this.state.firstname}
-            </Col>
+              </li>
+              <li className="list-group-item">
+                Nazwisko:{" "}
+                {this.state.lastname == "" ? "N/A" : this.state.lastname}
+              </li>
+              <li className="list-group-item">
+                Ulica i nr domu:{" "}
+                {this.state.street == "" ? "N/A" : this.state.street}
+              </li>
+              <li className="list-group-item">
+                Kod pocztowy:{" "}
+                {this.state.zip == "" ? "N/A" : this.state.zip}
+              </li>
+              <li className="list-group-item">
+                Miasto:{" "}
+                {this.state.city == "" ? "N/A" : this.state.city}
+              </li>
+            </ul>
+          </Col>
         </Row>
       </Container>
     );
